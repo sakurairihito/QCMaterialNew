a03b2c9b324f98a875e772fd22f7a08646d9831a
Documenter.jl <documenter@juliadocs.github.io>
2021-02-05 00:01:24 +0000

    build based on fe658c4

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = QCMaterial","category":"page"},{"location":"#QCMaterial","page":"Home","title":"QCMaterial","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [QCMaterial]","category":"page"},{"location":"#QCMaterial.dummy-Tuple{}","page":"Home","title":"QCMaterial.dummy","text":"Dummy function\n\n\n\n\n\n","category":"method"}]
+}
