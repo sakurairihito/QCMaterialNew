0b8f9dc719ee75f79952914662eb5960d257f147
Rihito Sakurai <sakurairihito@gmail.com>
2021-03-21 16:38:33 +0900

    add some test

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,3 +11,10 @@ end
     org_array = [1, 2, 3]
     @test all(doublefunc(org_array) .== 2 .* org_array)
 end
+
+
+@testset "uccsd.uccgsd" begin
+    # Construct 2-site Hubbard model
+    # Solve VQE
+    # Compare the results with the exact one
+end
