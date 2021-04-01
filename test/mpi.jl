ecc238c8971b074f7bdbdb38b124cd7e87e4fe23
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2021-04-02 08:48:41 +0900

    Add test/mpi.jl

---
+++ test/mpi.jl
@@ -0,0 +1,17 @@
+using MPI
+using Test
+using QCMaterial.mpi
+
+@testset "mpi.distribute" begin
+    size = 11
+    comm_size = 3
+
+    #rank = 0 
+    @test mpi.distribute(size, comm_size, 0) == (1, 4)
+
+    #rank = 1
+    @test mpi.distribute(size, comm_size, 1) == (5, 4)
+
+    #rank = 2
+    @test mpi.distribute(size, comm_size, 2) == (9, 3)
+end
