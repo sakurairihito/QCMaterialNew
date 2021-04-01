3b517019e489313803fa481360abd14ade95cc68
Hiroshi Shinaoka <h.shinaoka@gmail.com>
2021-04-02 08:40:23 +0900

    Add mpi.jl

---
+++ src/mpi.jl
@@ -0,0 +1,43 @@
+module mpi
+
+export distribute
+
+using MPI
+
+function distribute(size, comm_size, rank)
+    """
+    Compute the first element and size for a given rank
+
+    Parameters
+    ----------
+    size : Int
+        Length of array
+    comm_size : Int
+        Number of MPI  processes
+    rank: Int
+        MPI rank
+
+    Returns
+    -------
+    size : Int
+        Size
+    offset : Int
+        Offsets for the MPI process
+    """
+    if comm_size > size
+        error("comm_size is larger than size!")
+    end
+
+    base = div(size, comm_size)
+    leftover = size % comm_size
+    if rank+1 <= leftover
+        size = base+1
+        start = rank * (base+1) + 1
+    else
+        size = base
+        start = (base+1) * leftover + (rank-leftover) * base + 1
+    end
+    return start, size
+end
+
+end
