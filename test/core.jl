49fb257fa58e33d6e130f2f4105924cbe6c0c48e
Rihito Sakurai <sakurairihito@gmail.com>
2021-05-03 14:31:48 +0900

    add test/core.jl

--- test/core.jl
+++ test/core.jl
@@ -17,6 +17,40 @@ end
     @test terms_dict(op)[((1,"Y"), (2,"Y"))] == 2.0
 end
 
+
+@testset "core.qulacs_quantum_circuit" begin
+    n_qubit = 2
+    state = QulacsQuantumState(n_qubit, 0b00)
+    # Initial state |00>
+    #  (1) H gate to 0-th qubit
+    #  (2) CNOT gate (control 0, target 1)
+    #  (3) X gate to 0-th qubit
+    # Final state (|01> + |10>)/sqrt(2)
+    circuit = QulacsQuantumCircuit(n_qubit)
+    add_H_gate!(circuit, 0)
+    add_CNOT_gate!(circuit, 0, 1)
+    add_X_gate!(circuit, 0)
+    update_quantum_state!(circuit, state)
+
+    @test get_vector(state) ≈ [0, 1, 1, 0]/sqrt(2)
+end
+
+
+@testset "core.qulacs_quantum_state" begin
+    n_qubit = 2
+
+    # Initialize to 0b0
+    state = QulacsQuantumState(n_qubit)
+    state2 = QulacsQuantumState(n_qubit, 0b0)
+    @test get_vector(state) == get_vector(state2)
+
+    # Initialize to 0b01
+    state = QulacsQuantumState(n_qubit)
+    set_computational_basis!(state, 0b01)
+    state2 = QulacsQuantumState(n_qubit, 0b01)
+    @test get_vector(state) == get_vector(state2)
+end
+
 @testset "core.add_parametric_circuit_using_generator" begin
     n_qubit = 4
     a, i = 0, 0
