ca4b0d8c0685f50ab838d7943fcdbf808b2235ab
Rihito Sakurai <sakurairihito@gmail.com>
2021-05-12 15:17:32 +0900

    git add vqs.compute_C

--- src/vqs.jl
+++ src/vqs.jl
@@ -1,6 +1,7 @@
-export compute_Aij
+export compute_A
+export compute_C
 
-#A_ijの計算
+#Aの計算
 """
 Compute <phi (theta_bra) | phi(theta_ket)>
 """
@@ -24,7 +25,7 @@ function overlap(ucccirc::UCCQuantumCircuit, state0::QulacsQuantumState,
     res
 end
 
-function compute_Aij(ucccirc::UCCQuantumCircuit, state0::QulacsQuantumState, delta_theta=1e-8)
+function compute_A(ucccirc::UCCQuantumCircuit, state0::QulacsQuantumState, delta_theta=1e-8)
     num_thetas = num_theta(ucccirc)
     thetas = ucccirc.thetas
 
@@ -44,4 +45,45 @@ function compute_Aij(ucccirc::UCCQuantumCircuit, state0::QulacsQuantumState, del
         end
     end
     A
+end
+
+#Cの計算
+"""
+Compute <phi (theta_bra) |H| phi(theta_ket)>
+"""
+function transition(op::OFQubitOperator, ucccirc::UCCQuantumCircuit, state0::QulacsQuantumState,
+    thetas_left::Vector{Float64}, thetas_right::Vector{Float64})
+
+    circ_tmp = copy(ucccirc)
+
+    # Compute state_left
+    update_circuit_param!(circ_tmp, thetas_left)
+    state_left = copy(state0)
+    update_quantum_state!(circ_tmp, state_left)
+
+    # Compute state_right
+    update_circuit_param!(circ_tmp, thetas_right)
+    state_right = copy(state0)
+    update_quantum_state!(circ_tmp, state_right)
+    
+    # Compute <state_right|H|state_right>
+    res = get_transition_amplitude(op, state_left, state_right)
+    res
+end
+
+
+function compute_C(op::OFQubitOperator, ucccirc::UCCQuantumCircuit,state0::QulacsQuantumState, delta_theta=1e-8)
+    num_thetas = num_theta(ucccirc)
+    thetas = ucccirc.thetas
+
+    C = zeros(Complex{Float64}, num_thetas)
+    for i in 1:num_thetas
+        thetas_i = copy(ucccirc.thetas)
+        thetas_i[i] += delta_theta
+        C[i] = -real(
+            transition(op, ucccirc, state0, thetas, thetas_i)
+            -transition(op, ucccirc, state0, thetas, thetas)
+        )/delta_theta
+    end
+    C
 end
--- test/vqs.jl
+++ test/vqs.jl
@@ -2,7 +2,7 @@ using Test
 using LinearAlgebra
 using QCMaterial
 
-@testset "vqs.Aij" begin
+@testset "vqs.A" begin
     n_qubit = 2
     n_electron = 1
 
@@ -18,10 +18,28 @@ using QCMaterial
     new_thetas .+= 1e-8
     c_new = copy(c)
     update_circuit_param!(c_new, new_thetas)
-
-    println("debug", QCMaterial.overlap(c_new, state0, new_thetas, c.thetas))
-
-    A = compute_Aij(c, state0, 1e-2)
-    #println("A=",A)
+    A = compute_A(c, state0, 1e-2)
     @test A ≈ [0.25] atol=1e-5
+end
+
+@testset "vqs.C" begin
+    n_qubit = 2
+    n_electron = 1
+
+    ham = OFQubitOperator("X0 X1", 1.0)
+
+    c = UCCQuantumCircuit(n_qubit)
+    # a_0^dagger a_1 - a^1 a_0 -> 0.5i (X0 Y1 - X1 Y0)
+    generator = gen_t1(0, 1)
+    add_parametric_circuit_using_generator!(c, generator, 0.0)
+
+    state0 = QulacsQuantumState(n_qubit, 0b01)
+
+    new_thetas = copy(c.thetas)
+
+    new_thetas .+= 1e-8
+    c_new = copy(c)
+    update_circuit_param!(c_new, new_thetas)
+    C = compute_C(ham, c, state0, 1e-2)
+    @test C ≈ [0.5] atol=1e-5
 end
