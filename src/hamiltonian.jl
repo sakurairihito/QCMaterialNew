0618c654a1e6847251c4ed386410e68dfc931638
Rihito Sakurai <sakurairihito@gmail.com>
2021-06-30 20:37:49 +0900

    add impurity_ham_with_1imp_multibath

--- src/hamiltonian.jl
+++ src/hamiltonian.jl
@@ -1,14 +1,16 @@
 export generate_ham_1d_hubbard
+export generate_impurity_ham_with_1imp_multibath
 
 function generate_ham_1d_hubbard(t::Float64, U::Float64, nsite::Integer, μ::Float64)
+    #up_index,down_indexの定義は、QC_materialを参照。
+    #up = up_index(i)
+    #down = down_index(i)
+
     ham = FermionOperator()
-    
+
     #斥力項
     for i in 1:nsite
-        #up_index,down_indexの定義は、QC_materialを参照。
-        up = up_index(i)
-        down = down_index(i)
-        ham += FermionOperator("$(up)^ $(down)^ $(up) $(down)", -U) #左側に生成演算子。右際に消滅演算子をもっていく過程で半交換関係が1回でマイナスをつける。
+        ham += FermionOperator("$(up_index(i))^ $(down_index(i))^ $(up_index(i)) $(down_index(i))", -U) #左側に生成演算子。右際に消滅演算子をもっていく過程で半交換関係が1回でマイナスをつける。
     end
     #ホッピング項
     for i in 1:nsite-1
@@ -20,10 +22,36 @@ function generate_ham_1d_hubbard(t::Float64, U::Float64, nsite::Integer, μ::Flo
 
     #ケミカルポテンシャルの項
     for i in 1:nsite
-        up = up_index(i)
-        down = down_index(i)
-        ham += FermionOperator("$(up)^  $(up) ", -μ) 
-        ham += FermionOperator("$(down)^ $(down)", -μ)
+        ham += FermionOperator("$(up_index(i))^  $(up_index(i)) ", -μ) 
+        ham += FermionOperator("$(down_index(i))^ $(down_index(i))", -μ)
     end
+    ham
+end
+
+
+function generate_impurity_ham_with_1imp_multibath(U::Float64, V::Float64, μ::Float64, ε::Float64, nsite::Integer)
+    ham = FermionOperator()
+
+    #Coulomb   
+    ham += FermionOperator("$(up_index(1))^ $(down_index(1))^ $(up_index(1)) $(down_index(1))", -U)
+
+    #hybridization
+    for i in 1:nsite-1
+        ham += FermionOperator("$(up_index(i+1))^ $(up_index(i))", -V)
+        ham += FermionOperator("$(up_index(i))^ $(up_index(i+1))", -V)
+        ham += FermionOperator("$(down_index(i+1))^ $(down_index(i))", -V)
+        ham += FermionOperator("$(down_index(i))^ $(down_index(i+1))", -V)
+    end
+
+    #chemical potential
+    ham += FermionOperator("$(up_index(1))^ $(up_index(1))", -μ)
+    ham += FermionOperator("$(down_index(1))^ $(down_index(1))", -μ)
+
+    #bath energy level
+    for i in 2:nsite
+        ham += FermionOperator("$(up_index(i))^ $(up_index(i))", ε)
+        ham += FermionOperator("$(down_index(i))^ $(down_index(i))", ε)
+    end
+
     ham
 end
--- test/hamiltonian.jl
+++ test/hamiltonian.jl
@@ -10,6 +10,7 @@ using QCMaterial
 
     ham = generate_ham_1d_hubbard(t, U, nsite, μ)
     
+    
     ham_ref = FermionOperator()
     up1 = up_index(1)
     down1 = down_index(1)
@@ -31,6 +32,41 @@ using QCMaterial
     ham_ref += FermionOperator("$(down1)^ $(down1)", -μ)
     ham_ref += FermionOperator("$(up2)^  $(up2) ", -μ) 
     ham_ref += FermionOperator("$(down2)^ $(down2)", -μ)
+
+    @test ham == ham_ref
+end
+
+@testset "hamiltonian.generate_impurity_ham_with_1imp_multibath" begin
+    nsite = 2  
+    U = 1.0
+    V = 1.0
+    μ = 1.0
+    ε = 1.0
+
+    ham = generate_impurity_ham_with_1imp_multibath(U, V, μ, ε, nsite)
+
+    ham_ref = FermionOperator()
+    up1 = up_index(1)
+    down1 = down_index(1)
+    up2 = up_index(2)
+    down2 = down_index(2)
+
+    #Coulomb
+    ham_ref += FermionOperator("$(up1)^ $(down1)^ $(up1) $(down1)", -U) 
+    
+    #hybridization
+    ham_ref += FermionOperator("$(up2)^ $(up1)", -V) 
+    ham_ref += FermionOperator("$(up1)^ $(up2)", -V) 
+    ham_ref += FermionOperator("$(down2)^ $(down1)", -V) 
+    ham_ref += FermionOperator("$(down1)^ $(down2)", -V) 
+
+    #chemical potential
+    ham_ref += FermionOperator("$(up1)^  $(up1) ", -μ) 
+    ham_ref += FermionOperator("$(down1)^ $(down1)", -μ)
+
+    #bath energy level
+    ham_ref += FermionOperator("$(up2)^  $(up2) ", ε) 
+    ham_ref += FermionOperator("$(down2)^ $(down2)", ε)
 
     @test ham == ham_ref
 end
