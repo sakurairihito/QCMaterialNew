695b67996f9f54d574e0a750f0005000f6d356c1
Rihito Sakurai <sakurairihito@gmail.com>
2022-10-22 16:06:10 +0900

    debug for kucj's unit test

--- test/ucc.jl
+++ test/ucc.jl
@@ -107,7 +107,8 @@ end
     n_electron = 1
     c = UCCQuantumCircuit(n_qubit)
     # a_1^dagger a_2 - a^2 a_1 -> 0.5i (X1 Y2 - X2 Y1)
-    generator = gen_t2_kucj_2(2, 2, 1, 1)
+    # a_2^{dag} a_2 a_1{dag} a_1
+    generator = gen_t2_kucj_2(2, 1)
     generator_rm = rm_identity(generator)
     println("terms_dict(generator_rm)=", terms_dict(generator_rm))
     println("generator_rm.pyobj=", generator_rm.pyobj)
@@ -245,9 +246,9 @@ end
 
 @testset "ucc.gen_t2_kucj_2" begin
     #Random.seed!(1)
-    println("gen_t2_kucj_2(1,1,1,1)=", gen_t2_kucj_2(1, 1, 1, 1).pyobj.terms)
+    println("gen_t2_kucj_2(1,1,1,1)=", gen_t2_kucj_2(1, 1).pyobj.terms)
     #@test gen_t2_kucj_2(1, 1).pyobj.terms == Dict{Any,Any}()
-    println("gen_t2_kucj_2(2,2,1,1)=", gen_t2_kucj_2(2, 2, 1, 1).pyobj.terms)
+    println("gen_t2_kucj_2(2,2,1,1)=", gen_t2_kucj_2(2, 1).pyobj.terms)
     #gen_t2_kucj_2(2,2,1,1)=Dict{Any, Any}(() => 0.0 + 0.25im, ((0, "Z"),) => 0.0 - 0.25im, 
     #((0, "Z"), (1, "Z")) => 0.0 + 0.25im, ((1, "Z"),) => 0.0 - 0.25im)
     #@test gen_t2_kucj_2(2, 1).pyobj.terms == Dict{Any,Any}(() => 0.5 + 0.0im, ((0, "Z"),) => -0.5 + 0.0im, ((0, "Z"), (1, "Z")) => 0.5 + 0.0im, ((1, "Z"),) => -0.5 + 0.0im)
@@ -258,8 +259,8 @@ end
     n_electron = 1
     c = UCCQuantumCircuit(n_qubit)
     # a_1^dagger a_2 - a^2 a_1 -> 0.5i (X1 Y2 - X2 Y1)
-    generator = gen_t2_kucj_2(2, 1, 1, 2)
-    generator += gen_t2_kucj_2(4, 2, 2, 4)
+    generator = gen_t2_kucj_2(2, 1)
+    generator += gen_t2_kucj_2(4, 2)
     add_parametric_circuit_using_generator!(c, generator, 1.0)
     println("num_pauli(c, 1)=", num_pauli(c, 1))
     #@test num_pauli(c, 1) == 2 #１番目のパラメータに関するパウリ行列の個数
@@ -288,7 +289,7 @@ end
     println("num_pauli(c_test, 1)=", num_pauli(c_test, 1))
     println("thetaoffset_c_test=", c_test.theta_offsets)
 
-    generator_ = gen_t2_kucj_2(2, 1, 1, 2)
+    generator_ = gen_t2_kucj_2(2, 1)
     generator_ = rm_identity(generator_)
     #generator = rm_identity(generator), c.theta_offsets
     println("rm_identity_=", generator_)
@@ -306,61 +307,24 @@ end
     n_qubit = 2 * nsite
     U = 1.0
     t = -0.01
-    μ = 0
+    μ = 0.0
 
-    ham = FermionOperator()
-    for i in 1:nsite
-        up = up_index(i)
-        down = down_index(i)
-        ham += FermionOperator("$(up)^ $(down)^ $(up) $(down)", -U)
-    end
-
-    for i in 1:nsite-1
-        ham += FermionOperator("$(up_index(i+1))^ $(up_index(i))", t)
-        ham += FermionOperator("$(up_index(i))^ $(up_index(i+1))", t)
-        ham += FermionOperator("$(down_index(i+1))^ $(down_index(i))", t)
-        ham += FermionOperator("$(down_index(i))^ $(down_index(i+1))", t)
-    end
-
-    for i in 1:nsite
-        up = up_index(i)
-        down = down_index(i)
-        ham += FermionOperator("$(up)^  $(up) ", -μ)
-        ham += FermionOperator("$(down)^ $(down)", -μ)
-    end
+    ham = generate_ham_1d_hubbard(t, U, nsite, μ)
 
     n_electron = 2
     @assert mod(n_electron, 2) == 0
     sparse_mat = get_number_preserving_sparse_operator(ham, n_qubit, n_electron)
-
     enes_ed = eigvals(sparse_mat.toarray())
 
     ham_jw = jordan_wigner(ham)
-
-    circuit = kucj(n_qubit)
-    println("num_theta(circuit)=", num_theta(circuit))
-
-    function cost(theta_list)
-        state = create_hf_state(n_qubit, n_electron)
-        println("state=", state)
-        update_circuit_param!(circuit, theta_list)
-        println("update_circuit_param!(circuit, theta_list)=", update_circuit_param!(circuit, theta_list))
-        update_quantum_state!(circuit, state)
-        get_expectation_value(ham_jw, state)
-    end
-
-    theta_init = rand(num_theta(circuit))
-    cost_history = Float64[]
-    init_theta_list = theta_init
-    #println("init_theta_list=", init_theta_list)
-    push!(cost_history, cost(init_theta_list))
-    println("init_theta_list_after_cost=", init_theta_list)
-    method = "BFGS"
-    options = Dict("disp" => true, "maxiter" => 200, "gtol" => 1e-5)
-    callback(x) = push!(cost_history, cost(x))
-    opt = scipy_opt.minimize(cost, init_theta_list, method=method, callback=callback)
-    println("Eigval_vqe=", cost_history[end])
-    EigVal_min = minimum(enes_ed)
-    println("EigVal_min=", EigVal_min)
-    @test abs(EigVal_min - cost_history[end]) < 1e-3
+    state0 = create_hf_state(n_qubit, n_electron)
+    circuit, parameterinfo = kucj(n_qubit, k=1, sparse=false)
+    #println("num_theta(circuit)=", num_theta(circuit))
+    pinfo = QCMaterial.ParamInfo(parameterinfo)
+    theta_init = rand(pinfo.nparam)
+    cost_history, thetas_opt =
+    QCMaterial.solve_gs_kucj(ham_jw, circuit, state0, parameterinfo, theta_init=theta_init, verbose=true,
+        comm=QCMaterial.MPI_COMM_WORLD
+    )
+    @test abs(minimum(enes_ed) - cost_history[end]) < 1e-3
 end
